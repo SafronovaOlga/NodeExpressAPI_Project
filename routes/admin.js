c22c6f58b28c5fc514d7ae291ef8657eb0cb2c49
SafronovaOlga <104982522+SafronovaOlga@users.noreply.github.com>
2024-11-10 16:50:00 +0100

    Created Data and Logger files (#1)
    * Created User and Admin js.files
    
    * Created data and logger files

---
+++ routes/admin.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.get("/admin", getAdmin());
+// router.post("/admin", postAdmin());
+// router.get("/admin/:id", getAdminById());
+// //router.delete("/admin/:id", deleteAdminById());
+// router.patch("/admin/:id", patchAdminById());
+
+export default router;
