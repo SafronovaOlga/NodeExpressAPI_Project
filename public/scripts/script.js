974ee81698ccf9095e6314ef83229f7b549697ff
new_name <new_email>
2025-01-06 12:08:33 +0100

    Changed and added new code in the script.js file

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,11 +5,20 @@ const lastNameInput = document.getElementById('lastName');
 const ageInput = document.getElementById('age');
 const addButton = document.getElementById('addButton');
 const searchButton = document.getElementById('searchButton');
+const editButton = document.getElementById('editButton');
+const deleteButton = document.getElementById('deleteButton');
 const usersList = document.getElementById('usersList');
 const formAdd = document.getElementById('form-user');
 const formSearch = document.getElementById('form-search');
+const formEdit = document.getElementById('form-edit');
+const formDelete = document.getElementById('form-delete');
 
 let rowNumber = 1;
+
+const defaultIdPlaceholder = "Enter user ID...";
+const defaultFirstNamePlaceholder = "Enter first name...";
+const defaultLastNamePlaceholder = "Enter last name...";
+const defaultAgePlaceholder = "Enter age...";
 
 //mock data
 const usersFromData = [
@@ -116,18 +125,37 @@ class UI {
         }
     }
 
-    static addUserToList(user) {
+    static addUserToList(user, number) {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <th scope="row">${rowNumber}</th>
+            <th scope="row">${number}</th>
             <td>${user.firstName}</td>
             <td>${user.lastName}</td>
             <td>${user.age}</td>
             <td>${user.id}</td>
+            <td>
+            <i class="icon" id="editIcon">
+                    <a href="/edit" class="bi-pen">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+                        </svg>
+                    </a>
+                </i>
+            </td>
+            <td>
+                <i class="icon" id="deleteIcon">
+                    <a href="/delete" class="bi-trash">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                        </svg>
+                    </a>
+                </i>
+            </td>
         `;
 
         usersList.appendChild(row);
-        rowNumber ++;
+
     }
 
     static getSearchCriteria() {
@@ -188,23 +216,113 @@ class UI {
 
                         console.log("Found User: ", foundUser);
 
-                        const row = document.createElement('tr');
-                        row.innerHTML = `
-                            <th scope="row">${searchResultRowNumber}</th>
-                            <td>${user.firstName}</td>
-                            <td>${user.lastName}</td>
-                            <td>${user.age}</td>
-                            <td>${user.id}</td>
-                        `;
-
-                        usersList.appendChild(row);
+                        UI.addUserToList(foundUser, searchResultRowNumber);
                         searchResultRowNumber ++;
                     }
                 })
             }
+        }
+    }
 
+    static getRowText(event) {
+        const userRow = event.target.closest('tr');
+
+        let userInfo = [];
+
+        if(userRow) {
+            const userCells = userRow.cells;
+            for(let i = 1; i < 5; i++) {
+                userInfo[i-1] = userCells[i].textContent.trim();
+            }
         }
 
+        return userInfo;
+    }
+
+    static clearLocalStorage() {
+        if(localStorage.getItem('idValue') !== null) {
+            localStorage.removeItem('idValue');
+        }
+        if(localStorage.getItem('firstNameValue') !== null) {
+            localStorage.removeItem('firstNameValue');
+        }
+        if(localStorage.getItem('lastNameValue') !== null) {
+            localStorage.removeItem('lastNameValue');
+        }
+        if(localStorage.getItem('ageValue') !== null) {
+            localStorage.removeItem('ageValue');
+        }
+    }
+
+    static setValuesToLocalStorage(user) {
+        if(user !== null) {
+            localStorage.setItem('idValue', user.id);
+            localStorage.setItem('firstNameValue', user.firstName);
+            localStorage.setItem('lastNameValue', user.lastName);
+            localStorage.setItem('ageValue', user.age);
+        }
+    }
+
+    static fillPlaceholders() {
+        //Check if local storage data exists
+        const id = localStorage.getItem('idValue');
+        const firstName = localStorage.getItem('firstNameValue');
+        const lastName = localStorage.getItem('lastNameValue');
+        const age = localStorage.getItem('ageValue');
+
+        //Update placeholders
+        userIdInput.placeholder = id ? id : defaultIdPlaceholder;
+        firstNameInput.placeholder = firstName ? firstName : defaultFirstNamePlaceholder;
+        lastNameInput.placeholder = lastName ? lastName : defaultLastNamePlaceholder;
+        ageInput.placeholder = age ? age : defaultAgePlaceholder;
+    }
+
+    static activateEditButton() {
+        editButton.disabled = false;
+    }
+
+    static activateDeleteButton() {
+        if(userIdInput.placeholder !== defaultIdPlaceholder
+            && firstNameInput.placeholder !== defaultFirstNamePlaceholder
+            && lastNameInput.placeholder !== defaultLastNamePlaceholder
+            && ageInput.placeholder !== defaultAgePlaceholder
+        ) {
+            userIdInput.readOnly = true;
+            firstNameInput.readOnly = true;
+            lastNameInput.readOnly = true;
+            ageInput.readOnly = true;
+
+            deleteButton.disabled = false;
+        }
+    }
+
+    static getUpdatedUser() {
+        let updatedUser = {};
+        updatedUser.id = localStorage.getItem('idValue');
+
+        if(firstNameInput.value.toString().trim()) {
+            updatedUser.firstName = firstNameInput.value.toString().trim();
+        }
+        if(lastNameInput.value.toString().trim()) {
+            updatedUser.lastName = lastNameInput.value.toString().trim();
+        }
+        if(ageInput.value.toString().trim()) {
+            updatedUser.age = ageInput.value.toString().trim();
+        }
+
+        console.log("updatedUser = ", updatedUser);
+
+        return updatedUser;
+    }
+
+    static async editUser() {
+        const updatedUser = UI.getUpdatedUser();
+        await UserService.patchUsers(updatedUser);
+    }
+
+    static async deleteUser() {
+        const id = localStorage.getItem('idValue');
+        await UserService.deleteUsers(id);
     }
 
 }
@@ -300,6 +418,91 @@ class UserService {
             throw error;
         }
     }
+
+    static async patchUsers(user) {
+        if (!user.id || (!user.firstName && !user.lastName && user.age === undefined)) {
+            console.error("[ERROR] Invalid parameters. 424")
+            throw new Error("Invalid parameters.");
+        }
+
+        let body = {};
+        if(user.firstName) {
+            body.firstName = user.firstName;
+        }
+        if(user.lastName) {
+            body.lastName = user.lastName;
+        }
+        if(user.age) {
+            body.age = user.age;
+        }
+
+        console.log("body = ", body);
+
+        try {
+            const response = await fetch(
+                `http://localhost:5076/api/users/${user.id}`,
+                {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(
+                        body
+                    )
+                })
+
+            if (response.status !== 200) {
+                console.error("[ERROR] Response status:", response.status);
+                throw new Error("Failed to post users.");
+            }
+
+            const contentType = response.headers.get('Content-Type');
+
+            if (contentType.includes('text/html')) {
+
+                return await response.text();
+            } else {
+                console.error("[ERROR] Unexpected Content-Type: ", contentType);
+                throw new Error("Unexpected Content-Type.");
+            }
+        } catch (error) {
+            console.error("Fetch error: ", error);
+            throw error;
+        }
+    }
+
+    static async deleteUsers(id) {
+        if (!id) {
+            console.error("[ERROR] Invalid parameters.")
+            throw new Error("Invalid parameters.");
+        }
+
+        try {
+            const response = await fetch(
+                `http://localhost:5076/api/users/${id}`,
+                {
+                    method: 'DELETE'
+                })
+
+            if (response.status !== 200) {
+                console.error("[ERROR] Response status:", response.status);
+                throw new Error("Failed to post users.");
+            }
+
+            const contentType = response.headers.get('Content-Type');
+
+            if (contentType.includes('text/html')) {
+
+                return await response.text();
+            } else {
+                console.error("[ERROR] Unexpected Content-Type: ", contentType);
+                throw new Error("Unexpected Content-Type.");
+            }
+        } catch (error) {
+            console.error("Fetch error: ", error);
+            throw error;
+        }
+    }
 }
 
 //event to show App Name
@@ -310,6 +513,7 @@ document.addEventListener('DOMContentLoaded', UI.displayUsers);
 
 //we are on tab Add
 if(formAdd !== null) {
+    UI.clearLocalStorage();
     //event to activate Add button
     formAdd.addEventListener('input', UI.activateAddButton);
 
@@ -321,15 +525,18 @@ if(formAdd !== null) {
         event.preventDefault();
 
         const user = await UI.createUser();
-        UI.addUserToList(user);
+        UI.addUserToList(user, rowNumber);
+        rowNumber ++;
 
         formAdd.reset();
         addButton.disabled = true;
     })
 }
 
+
 //we are on tab Search
 if(formSearch !== null) {
+    UI.clearLocalStorage();
     formSearch.addEventListener('input', UI.activateSearchButton);
 
     formSearch.addEventListener('submit', async (event) => {
@@ -341,5 +548,19 @@ if(formSearch !== null) {
         formSearch.reset();
         searchButton.disabled = true;
     })
-
 }
+
+
+//we are on any tab
+usersList.addEventListener('click', (event) => {
+    console.log(event.target);
+    if(event.target.classList.contains('bi-pen') || event.target.classList.contains('bi-trash')) {
+        const userInfo = UI.getRowText(event);
+        const copiedUser = new User(userInfo[0], userInfo[1], userInfo[2], userInfo[3]);
+
+        console.log("copiedUser = ", copiedUser);
+
+        UI.clearLocalStorage();
+        UI.setValuesToLocalStorage(copiedUser);
+    }
+})
